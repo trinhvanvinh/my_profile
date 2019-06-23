20f0f5e726df9d9c0f310c871b2dfe2b6eea5778
trinhvanvinh <trinhvanvinh018@gmail.com>
2019-06-23 23:04:41 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>hello vinh đẹp trai, hi thảo 56,25kg</h1>
       </header>
     </div>
   );
